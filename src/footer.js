15753178e55c1db1d92b2419c45edc682fbbcb1d
harshdsdh <harsh.dsdh@gmail.com>
2019-08-08 16:18:19 -0400

    a

--- src/footer.js
+++ src/footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
 const handleSubmit = async e => {
   e.preventDefault();
   axios
-    .post("/", {
+    .post("http://localhost:/" + PORT, {
       headers: { "Access-Control-Allow-Origin": "*" },
       name: document.getElementsByName("name")[0].value,
       email: document.getElementsByName("email")[0].value,
