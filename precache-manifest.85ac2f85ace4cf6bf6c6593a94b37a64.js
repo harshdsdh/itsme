801cae09533c298758f6cd9400bc02720c5228dd
harshdsdh <harsh.dsdh@gmail.com>
2020-03-08 23:13:06 -0400

    Updates

---
+++ precache-manifest.85ac2f85ace4cf6bf6c6593a94b37a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c4f990a24b57a0e7d6b5be707449f2",
+    "url": "/itsme/index.html"
+  },
+  {
+    "revision": "2d88c73d817c46ccab20",
+    "url": "/itsme/static/css/main.2f9cf734.chunk.css"
+  },
+  {
+    "revision": "e7d3f17fa5488e74c7fd",
+    "url": "/itsme/static/js/2.abc9f76b.chunk.js"
+  },
+  {
+    "revision": "2d88c73d817c46ccab20",
+    "url": "/itsme/static/js/main.77aa6c9b.chunk.js"
+  },
+  {
+    "revision": "ea7ed1e52b0baaf2284c",
+    "url": "/itsme/static/js/runtime~main.3d18bf98.js"
+  },
+  {
+    "revision": "47455ba52fa7cfb9f713e61a82fd0584",
+    "url": "/itsme/static/media/mngif.47455ba5.gif"
+  }
+]);
